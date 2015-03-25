6effb08ce8bfc148ee4c68c312313a68b0df4499
Prashant Singh <prash471@gmail.com>
2015-03-26 01:06:15 +0530

    Added examples implemented radial layout 2

--- Layout/Radial.hs
+++ Layout/Radial.hs
@@ -2,8 +2,9 @@
 
 module Layout.Radial
        ( 
-        radialLayout
-       , renderTree
+        radialLayout1
+       ,radialLayout2
+       ,renderTree
        ) where
 
 import Data.Tree
@@ -17,15 +18,30 @@ import Diagrams.Prelude
 import Data.Function       (on)
 
 ---------------------------------------------------------
--- Radial Layout Implementation 
+-- Radial Layout Implementation 1: All children on circle centered at parent
 
 radialLayout :: Tree a -> Tree (a,P2)
-radialLayout t = unRelativizeRadial (mkP2 1 1) t
+radialLayout t = unRelativizeRadial1 (mkP2 0 0) t
 
-unRelativizeRadial :: P2 -> Tree a -> Tree(a,P2)
-unRelativizeRadial curPt (Node a ts) = Node (a, curPt) (L.zipWith (\v t -> unRelativizeRadial (curPt .+^ v) t) vs ts)
+unRelativizeRadial1 :: P2 -> Tree a -> Tree(a,P2)
+unRelativizeRadial1 curPt (Node a ts) = Node (a, curPt) (L.zipWith (\v t -> unRelativizeRadial1 (curPt .+^ v) t) vs ts)
   where	L.map (scale (1/len)) (L.map (rotateBy (1/fromIntegral len/2)) (K.iterateN len (rotateBy (1/fromIntegral len)) unit_X))
         len  = fromIntegral (L.length ts)
+
+---------------------------------------------------------
+-- Radial Layout Implementation 2: All children on circle centered at root of the tree in expanding form
+
+foo :: P2 -> P2
+foo k = mkP2 (r * cos t) (r * sin t)
+  where r   = (-1) * snd (unp2 (k))
+        t   = 2 * pi * fst (unp2 (k)) / 64
+        -- scale as 64: depending upon tree width
+
+radialLayout2 :: Tree a -> Tree (a,P2)
+radialLayout2 t = unRelativizeRadial2 t
+
+unRelativizeRadial2 :: Tree (a,P2) -> Tree(a,P2)
+unRelativizeRadial2 tree =  fmap (\x -> (fst x, foo (snd x))) tree
 
 ----------------------------------------------------------------------
 -- Tree rendering
