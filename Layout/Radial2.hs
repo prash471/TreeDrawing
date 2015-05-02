1247b15bb0848add8db2b2f52f494da97213fba6
Prashant Singh <prash471@gmail.com>
2015-05-03 01:29:38 +0530

    Update Radial2.hs

--- Layout/Radial2.hs
+++ Layout/Radial2.hs
@@ -15,6 +15,10 @@ t1 = Node 'A' [Node 'B' (L.map lf "CD"), Node 'I' (L.map lf "JK")] where lf x = 
 decorateDepth:: Int -> Tree a -> Tree (a,Int)
 decorateDepth d (Node a ts) = Node (a,d) $ L.map (decorateDepth (d+1)) ts
 
+
+radialLayoutstart:: Tree a -> Angle -> Angle -> Tree (a,P2)
+radialLayoutstart t1 alpha beta = radialLayout (decorateDepth t1) alpha beta (countLeaves t1) 
+
 radialLayout :: Tree (a,Int) -> Angle -> Angle -> Int -> Tree (a, P2)
 radialLayout (Node (a,d) ts) alpha beta k = Node (a,pt) ts # map (countChildren alpha beta r k) ts   
 	where 	pt 	= mkP2 (d * cos (theta + u)/2) (d * sin (theta + u)/2)
@@ -29,6 +33,3 @@ countChildren :: Double -> Angle -> Angle -> Tree (a,Int) -> Tree (a,P2)
 countChildren alpha beta r k (Node (a,d) ts) k =
 	if L.length ts > 0 then  radialLayout (Node (a,d) ts) alpha beta k
 	otherwise return  
-
-radialLayoutstart:: Tree a -> Angle -> Angle -> Tree (a,P2)
-radialLayoutstart t1 alpha beta = radialLayout t1 alpha beta (countLeaves t1) 
