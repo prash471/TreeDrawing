5aec16aa6c02b08a05b6acedece7bfd8346ec8a6
Prashant Singh <prash471@gmail.com>
2015-05-03 09:07:30 +0530

    Update Radial2.hs

--- Layout/Radial2.hs
+++ Layout/Radial2.hs
@@ -15,16 +15,20 @@ t1 = Node 'A' [Node 'B' (L.map lf "CD"), Node 'I' (L.map lf "JK")] where lf x = 
 decorateDepth:: Int -> Tree a -> Tree (a,Int)
 decorateDepth d (Node a ts) = Node (a,d) $ L.map (decorateDepth (d+1)) ts
 
-radialLayoutstart:: Tree a -> Angle -> Angle -> Tree (a,P2)
-radialLayoutstart t1 alpha beta = radialLayout (decorateDepth t1) alpha beta (countLeaves t1) 
+radialLayoutstart:: Tree a -> Double -> Double -> Tree (a,P2)
+radialLayoutstart t1 alpha beta = radialLayout (decorateDepth 0 t1) alpha beta (countLeaves t1) 
 
-radialLayout :: Tree (a,Int) -> Angle -> Angle -> Int -> Tree (a, P2)
-radialLayout (Node (a,d) []) alpha beta k = 
-radialLayout (Node (a,d) ts) alpha beta k = Node (a,pt) ts # map (radialLayout alpha beta k) ts   
-	where 	pt 	= mkP2 (d * cos (theta + u)/2) (d * sin (theta + u)/2)
-  		u       = theta + (beta - alpha) * lambda / k  
-        	lambda  = countLeaves t 
+radialLayout :: Tree (a,Int) -> Double -> Double -> Int -> Tree (a, P2)
+radialLayout (Node (a,d) []) alpha beta k = Node (a,pt) []
+	where 	pt 	= mkP2 (fromIntegral d * cos (theta + u)/2) (fromIntegral d * sin (theta + u)/2)
+  		u       = theta
         	theta 	= alpha 
 
-countLeaves :: Tree (a,P2) -> Int 
+radialLayout (Node (a,d) ts) alpha beta k = Node (a,pt) ts # L.map (radialLayout alpha beta fromIntegral k) ts   
+	where 	pt 	= mkP2 (fromIntegral d * cos (theta + u)/2) (fromIntegral d * sin (theta + u)/2)
+  		u       = theta + (beta - alpha) * fromIntegral lambda / fromIntegral k  
+        	lambda  = countLeaves (Node (a,d) ts)
+        	theta 	= alpha 
+
+countLeaves :: Tree (a,Int) -> Int 
 countLeaves t = L.length $ L.last (L.map (L.map rootLabel) $ L.takeWhile (not . L.null) $ iterate (L.concatMap subForest) [t])
