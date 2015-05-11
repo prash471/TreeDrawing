9f97d795e9c1fb6d61cd6d370ed03e2f73202448
Prashant Singh <prash471@gmail.com>
2015-05-11 21:58:46 +0530

    Update Radial2.hs

--- Layout/Radial2.hs
+++ Layout/Radial2.hs
@@ -16,15 +16,15 @@ decorateDepth:: Int -> Tree a -> Tree (a,Int)
 decorateDepth d (Node a ts) = Node (a,d) $ L.map (decorateDepth (d+1)) ts
 
 radialLayoutstart:: Tree a -> Double -> Double -> Tree (a,P2)
-radialLayoutstart t1 alpha beta = radialLayout (decorateDepth 0 t1) alpha beta (countLeaves t1) 
+radialLayoutstart t1 alpha beta = radialLayout alpha beta (countLeaves (decorateDepth 0 t1)) (decorateDepth 0 t1)
 
-radialLayout :: Tree (a,Int) -> Double -> Double -> Int -> Tree (a, P2)
-radialLayout (Node (a,d) []) alpha beta k = Node (a,pt) []
+radialLayout :: Double -> Double -> Int -> Tree (a,Int) ->  Tree (a, P2)
+radialLayout alpha beta k (Node (a,d) []) = Node (a,pt) []
 	where 	pt 	= mkP2 (fromIntegral d * cos (theta + u)/2) (fromIntegral d * sin (theta + u)/2)
   		u       = theta
         	theta 	= alpha 
 
-radialLayout (Node (a,d) ts) alpha beta k = Node (a,pt) ts # L.map (radialLayout alpha beta fromIntegral k) ts   
+radialLayout alpha beta k (Node (a,d) ts) = Node (a,pt) # L.map (radialLayout alpha beta k) ts   
 	where 	pt 	= mkP2 (fromIntegral d * cos (theta + u)/2) (fromIntegral d * sin (theta + u)/2)
   		u       = theta + (beta - alpha) * fromIntegral lambda / fromIntegral k  
         	lambda  = countLeaves (Node (a,d) ts)
