b17e0ab19865453c29aebea5b17e57495c3bede4
Prashant Singh <prash471@gmail.com>
2015-05-14 12:20:20 +0530

    Update Radial2.hs

--- Layout/Radial2.hs
+++ Layout/Radial2.hs
@@ -12,23 +12,24 @@ import Data.Function       (on)
 
 t1 = Node 'A' [Node 'B' (L.map lf "CD"), Node 'I' (L.map lf "JK")] where lf x = Node x []
 
-decorateDepth:: Int -> Tree a -> Tree (a,Int)
-decorateDepth d (Node a ts) = Node (a,d) $ L.map (decorateDepth (d+1)) ts
+radialLayoutstart:: Tree a -> Double -> Double -> Tree (a,P2,Int)
+radialLayoutstart t alpha beta = radialLayout alpha beta alpha (countLeaves (decorateDepth 0 t)) (decorateDepth 0 t)
 
-radialLayoutstart:: Tree a -> Double -> Double -> Tree (a,P2)
-radialLayoutstart t1 alpha beta = radialLayout alpha beta (countLeaves (decorateDepth 0 t1)) (decorateDepth 0 t1)
+radialLayout :: Double -> Double -> Double -> Int -> Tree (a,P2,Int) ->  Tree (a, P2, Int)
+radialLayout alpha beta theta k (Node (a,pt,d) ts) = Node (a,pt,d) (L.map (foo alpha beta theta k) ts)
 
-radialLayout :: Double -> Double -> Int -> Tree (a,Int) ->  Tree (a, P2)
-radialLayout alpha beta k (Node (a,d) []) = Node (a,pt) []
-	where 	pt 	= mkP2 (fromIntegral d * cos (theta + u)/2) (fromIntegral d * sin (theta + u)/2)
-  		u       = theta
-        	theta 	= alpha 
+foo :: Double -> Double -> Double -> Int -> Tree (a,P2,Int) -> Tree (a,P2,Int)
+foo alpha beta theta k (Node (a,pt,d) ts)	= do	
+			let lambda  = countLeaves (Node (a,pt,d) ts) 	
+			let u       = theta + (beta - alpha) * fromIntegral lambda / fromIntegral k
+			if (L.length ts) > 0 then L.map (radialLayout alpha beta u k) ts else []
+			--return (a,mkP2 (fromIntegral (d) * cos (theta + u)/2) (fromIntegral (d) * sin (theta + u)/2),d)
 
-radialLayout alpha beta k (Node (a,d) ts) = Node (a,pt) (L.map (radialLayout alpha beta k) ts)   
-	where 	pt 	= mkP2 (fromIntegral d * cos (theta + u)/2) (fromIntegral d * sin (theta + u)/2)
-  		u       = theta + (beta - alpha) * fromIntegral lambda / fromIntegral k  
-        	lambda  = countLeaves (Node (a,d) ts)
-        	theta 	= alpha 
+decorateDepth:: Int -> Tree a -> Tree (a,P2,Int)
+decorateDepth d (Node a ts) = Node (a,mkP2 0 0,d) $ L.map (decorateDepth (d+1)) ts
 
-countLeaves :: Tree (a,Int) -> Int 
+countLeaves :: Tree (a,P2,Int) -> Int 
 countLeaves t = L.length $ L.last (L.map (L.map rootLabel) $ L.takeWhile (not . L.null) $ iterate (L.concatMap subForest) [t])
+
+finalTree :: Tree (a,P2,Int) -> Tree (a,P2)
+finalTree (Node (a,pt,d) ts) = Node (a,pt) $ L.map finalTree ts
