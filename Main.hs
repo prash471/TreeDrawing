58701d4602a2c6dd727153037509826a8ef74693
Prashant Singh <prash471@gmail.com>
2015-05-22 17:06:38 +0530

    Added Test Cases

--- Main.hs
+++ Main.hs
@@ -4,7 +4,10 @@ import Diagrams.Prelude
 import Data.Tree
 import Diagrams.Backend.SVG.CmdLine
 
-t1 = Node 'A' [Node 'B' [], Node 'C'[], Node 'D'[], Node 'E'[], Node 'F'[], Node 'G'[], Node 'H'[], Node 'I'[] ]
+t1 = Node 'A' [Node 'B' (L.map lf "CDQXXXXXXXX"), Node 'I' (L.map lf "CDQ"), Node 'L' (L.map lf "CDQ")] where lf x = Node x []
+--t1 = Node 'A' [Node 'B' (L.map lf "CDQXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"), Node 'I' [Node 'J' (L.map lf "CDQXXXX")]] where lf x = Node x []
+--t1 = Node 'A' [Node 'B' [], Node 'C'[], Node 'D'[], Node 'E'[], Node 'F'[], Node 'G'[], Node 'H'[], Node 'I'[] ]
+
 example =
    renderTree (\n -> (text (show n) # fontSizeG 0.5
                             <> circle 0.5 # fc white))
